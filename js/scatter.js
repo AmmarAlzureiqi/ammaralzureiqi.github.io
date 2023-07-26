469a27b710d4cd05286cea5363246007f07c4e06
AmmarAlzureiqi <100096699+AmmarAlzureiqi@users.noreply.github.com>
2023-07-26 19:03:02 -0400

    n

--- js/scatter.js
+++ js/scatter.js
@@ -1,9 +1,9 @@
-// set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 30, left: 60},
-width = 600 - margin.left - margin.right,
-height = 600 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+  width = 600 - margin.left - margin.right,
+  height = 600 - margin.top - margin.bottom;
+
+
 var svg = d3.select("#my_dataviz")
 .append("svg")
 .attr("width", width + margin.left + margin.right)
