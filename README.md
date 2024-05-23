2a4afe42661f28eed6cf9b9be7e11db2a76410a6
= <=>
2024-05-24 02:19:10 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# Ammar-Web
+# Portfolio Website
+
+Welcome to my portfolio website! This site showcases my work, skills, and projects. You can visit the live site by clicking the link below:
+
+[Visit My Portfolio](https://ammaralzureiqi.github.io/Ammar-Website)
+
+## About
+
+This portfolio website includes:
+
+- An overview of my skills and expertise
+- A showcase of my recent projects
+- Information about my professional experience
+- Contact information
+
+## Features
+
+- **Responsive Design**: The website is fully responsive and works well on all devices.
+- **Project Showcase**:  Descriptions and images of my projects.
+- **Contact Form**: A simple form for visitors to get in touch with me.
+
+## Technologies Used
+
+- React.js
+- Three.js
+- Framer Motion
+- TailwindCSS
+
+
+
---
+++ src/constants/index.js
@@ -0,0 +1,281 @@
+import {
+  frontend,
+  backend,
+  ux,
+  prototyping,
+  javascript,
+  html,
+  css,
+  reactjs,
+  tailwind,
+  nodejs,
+  git,
+  figma,
+  docker,
+  komikult,
+  python,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  space,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+  aws,
+  R,
+  mysql,
+  tableau,
+  pytorch,
+  flask,
+  mongodb,
+  powerbi,
+  datascience,
+  statistics,
+  datavizualization,
+  western,
+  mkbookkeeping,
+  uiuc,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Data Science',
+    icon: datascience,
+  },
+  {
+    title: 'Statistics',
+    icon: statistics,
+  },
+  {
+    title: 'Backend Developer',
+    icon: backend,
+  },
+  {
+    title: 'Data Vizualization',
+    icon: datavizualization,
+  },
+];
+
+const technologies = [
+  {
+    name: 'Python',
+    icon: python,
+  },
+  {
+    name: 'R',
+    icon: R,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'tableau',
+    icon: tableau,
+  },
+  {
+    name: 'powerbi',
+    icon: powerbi,
+  },
+  {
+    name: 'pytorch',
+    icon: pytorch,
+  },
+  {
+    name: 'mysql',
+    icon: mysql,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'aws',
+    icon: aws,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'figma',
+    icon: figma,
+  },
+  {
+    name: 'docker',
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Bachelor's of Statistics",
+    company_name: 'Western University',
+    icon: western,
+    iconBg: '#333333',
+    date: 'Sep 2017 - May 2021',
+  },
+  {
+    title: 'Software Developer Intern',
+    company_name: 'Western University',
+    icon: western,
+    iconBg: '#333333',
+    date: 'Apr 2019 - Aug 2019',
+  },
+  {
+    title: 'Software Developer Intern',
+    company_name: 'MK Bookkeeping and Tax Services',
+    icon: mkbookkeeping,
+    iconBg: '#333333',
+    date: 'Apr 2020 - Aug 2020',
+  },
+  {
+    title: "Master's of Computer Science in Data Science",
+    company_name: 'University of Illinois Urbana Champaign',
+    icon: uiuc,
+    iconBg: '#333333',
+    date: 'May 2023 - Dec 2024',
+  },
+];
+
+const projects = [
+  {
+    id: 'project-1',
+    name: 'KomiKult',
+    description: 'A comic characters list app that displays Marvel characters.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'mongodb',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: komikult,
+    repo: 'https://github.com/shaqdeff/KomiKult',
+    demo: 'https://shaqdeff.github.io/KomiKult/',
+  },
+  {
+    id: 'project-2',
+    name: 'Leaderboard',
+    description:
+      'A leaderboard list app that displays scores submitted by different players.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'restapi',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'scss',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: leaderboard,
+    repo: 'https://github.com/shaqdeff/Leaderboard',
+    demo: 'https://shaqdeff.github.io/Leaderboard/',
+  },
+  {
+    id: 'project-3',
+    name: 'Math Magicians',
+    description: 'This is a single-page calculator app built with React',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: math,
+    repo: 'https://github.com/shaqdeff/Math-Magicians',
+    demo: 'https://inspiring-medovik-37d3b3.netlify.app/',
+  },
+  {
+    id: 'project-4',
+    name: 'Movie Metro',
+    description: `A single-page application that allows users to search for any movie or show's ratings and its details.`,
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: movie,
+    repo: 'https://github.com/shaqdeff/Movie-Metro',
+    demo: 'https://movie-metro.netlify.app/',
+  },
+  {
+    id: 'project-5',
+    name: 'Nyeusi Fest Site',
+    description:
+      'This is a demo concert website for a music festival called Nyeusi.',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: nyeusi,
+    repo: 'https://github.com/shaqdeff/Nyeusi-Fest-Site',
+    demo: 'https://shaqdeff.github.io/Nyeusi-Fest-Site/',
+  },
+];
+
+export { services, technologies, experiences, projects };
