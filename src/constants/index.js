4644989f74d8005e3552d21f4e4c39e8786fa755
= <=>
2024-05-24 03:50:56 +0300

    initial commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -188,26 +188,26 @@ const projects = [
   },
   {
     id: 'project-2',
-    name: 'Leaderboard',
+    name: 'Mood of Music',
     description:
       'A leaderboard list app that displays scores submitted by different players.',
-    tags: [
-      {
-        name: 'react',
-        color: 'blue-text-gradient',
-      },
-      {
-        name: 'restapi',
-        color: 'green-text-gradient',
-      },
-      {
-        name: 'scss',
-        color: 'pink-text-gradient',
-      },
-    ],
+    // tags: [
+    //   {
+    //     name: 'react',
+    //     color: 'blue-text-gradient',
+    //   },
+    //   {
+    //     name: 'restapi',
+    //     color: 'green-text-gradient',
+    //   },
+    //   {
+    //     name: 'scss',
+    //     color: 'pink-text-gradient',
+    //   },
+    // ],
     image: leaderboard,
-    repo: 'https://github.com/shaqdeff/Leaderboard',
-    demo: 'https://shaqdeff.github.io/Leaderboard/',
+    repo: 'https://github.com/AmmarAlzureiqi/Mood-of-Music',
+    demo: 'https://github.com/AmmarAlzureiqi/Mood-of-Music',
   },
   {
     id: 'project-3',
