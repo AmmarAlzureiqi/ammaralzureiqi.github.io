10972c41be6bdc5ff054cc34bcc7a0ef0e9288f7
= <alzureiqi2@gmail.com>
2025-01-23 22:42:22 -0500

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Inter } from 'next/font/google'
-import 'globals.css'
+import './globals.css'
 import { Sidebar } from '../components/sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
